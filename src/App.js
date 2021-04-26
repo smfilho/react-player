537f939c87946471ccc9986092b619bbd7debd4e
Silvio Melo Filho <smfilho@gmail.com>
2021-04-25 23:37:37 -0500

    Last update before new feats

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+// Styles
+import './styles/app.scss';
+// Components
+import Player from './components/Player'
+import Song from './components/Song'
+// Data
+import data from './data';
+
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Title</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;

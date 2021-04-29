44c7369b3c7714c54b045b983191d494f0c9a9f3
Silvio Melo Filho <smfilho@gmail.com>
2021-04-28 23:20:38 -0500

    some styling

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,29 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Player = () => {
   return (
-    <div className="player">
-      <div className="time-control">
-        <p>Start Time</p>
-        <input type="range" />
-        <p>End Time</p>
+    <div className='player'>
+      <div className='time-control'>
+        <p className='p_start-time'>Start Time</p>
+        <input type='range' />
+        <p className='p_end-time'>End Time</p>
       </div>
-      <div className="play-control">
-        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
-        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
+      <div className='play-control'>
+        <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
+        <FontAwesomeIcon className='play' size='2x' icon={faPlay} />
+        <FontAwesomeIcon
+          className='skip-forward'
+          size='2x'
+          icon={faAngleRight}
+        />
       </div>
     </div>
   );
 };
 
-export default Player; 
+export default Player;

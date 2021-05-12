4302c5e6b859a894a1ac7f5e0e9884717be0cc6e
Silvio Melo Filho <smfilho@gmail.com>
2021-05-11 22:53:40 -0500

    Fixed play/pause button

--- src/components/Player.js
+++ src/components/Player.js
@@ -61,7 +61,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
           onClick={playSongHandler}
           className='play'
           size='2x'
-          icon={isPlaying ? faPlay : faPause}
+          icon={isPlaying ? faPause : faPlay}
         />
         <FontAwesomeIcon
           className='skip-forward'

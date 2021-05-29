b8f1c362efecade7578abc024589cc852c968a23
Silvio Melo Filho <smfilho@gmail.com>
2021-05-28 21:38:37 -0500

    Done: Progress bar(input) styling

--- src/components/Player.js
+++ src/components/Player.js
@@ -62,7 +62,12 @@ const Player = ({
     <div className='player'>
       <div className='time-control'>
         <p>{getTime(songInfo.currentTime)}</p>
-        <div className='track'>
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})`,
+          }}
+          className='track'
+        >
           <input
             min={0}
             max={songInfo.duration || 0}
